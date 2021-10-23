97f7be5f7ca680db117c1035fabadd9487c8bb5e
ilias000 <iliaspiot@yahoo.gr>
2021-10-23 13:36:19 +0300

    Initial Commit.

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int id = fork();
+    int n;
+    if (id == 0)
+    {
+        n = 1;
+    }
+    else
+    {
+        n = 6;
+    }
+    if (id != 0)
+    {
+        wait(NULL);
+    }
+
+    for (int i = n; i < n + 5; i++)
+    {
+        cout << i << " ";
+        fflush(stdout);
+    }
+
+    if (id != 0)
+    {
+        cout << endl;
+    }
+
+    return 0;
+}
